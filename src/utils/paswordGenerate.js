16f0b183239db2b596df8e643b52dec562b236e4
Pavel Frolov <Frolov-Works@yandex.ru>
2022-08-14 18:30:59 +0300

    fix: passwordGenerate

--- src/utils/paswordGenerate.js
+++ src/utils/paswordGenerate.js
@@ -12,5 +12,48 @@ export const passwordGenerate = (
   let numeric = isNumeric ? '0' : ''
   let specialCharacters = isSpecialCharacters ? '?' : ''
 
-  return randomize(`${lowerCase}${upperCase}${numeric}${specialCharacters}`, lengthPassword, {chars: '#$@!'})
+  let password = randomize(`${lowerCase}${upperCase}${numeric}${specialCharacters}`, lengthPassword, {chars: '#$@!'})
+
+
+  // эти функции для того что-бы минимум 1 символ lowerCase upperCase numeric specialCharacters был в пароле
+  if(isLowerCase) password = setOneLowerCase(password)
+  if(isUpperCase) password = setOneUpperCase(password)
+  if(isNumeric) password = setOneNumeric(password)
+  if(isSpecialCharacters) password = setOneSpecialCharacters(password)
+
+  return password
+}
+
+function setOneSpecialCharacters (password) {
+  const specialCharacters = ['#', '$', '@', '!']
+  const randomSpecialCharacters = Math.floor(Math.random() * specialCharacters.length)
+  let passwordArr = password.split('')
+
+  passwordArr[Math.ceil(passwordArr.length / 2)  + 1] = specialCharacters[randomSpecialCharacters]
+  console.log(specialCharacters[randomSpecialCharacters])
+  console.log(passwordArr)
+  return passwordArr.join('')
+}
+
+function setOneNumeric (password) {
+  const randomNumber = Math.floor(Math.random() * 10)
+  let passwordArr = password.split('')
+
+  passwordArr[Math.floor(passwordArr.length / 2)] = randomNumber
+  return passwordArr.join('')
+}
+
+function setOneLowerCase (password) {
+  const randomLetter = (Math.floor(Math.random() * (25 - 0 + 1) + 0) + 10).toString(36)
+  let passwordArr = password.split('')
+
+  passwordArr[Math.floor(passwordArr.length / 2) - 1] = randomLetter
+  return passwordArr.join('')
+}
+
+function setOneUpperCase (password) {
+  const randomLetter = (Math.floor(Math.random() * (25 - 0 + 1) + 0) + 10).toString(36)
+  let passwordArr = password.split('')
+  passwordArr[Math.floor(passwordArr.length / 2) - 2] = randomLetter.toUpperCase()
+  return passwordArr.join('')
 }
